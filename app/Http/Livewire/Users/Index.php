b0e2ab2f538e9605dbd42d69f6ffb8cb922cf8e0
ADMIN <olorundaolaoluwa@gmail.com>
2024-11-08 12:03:51 +0100

    smtp

--- app/Http/Livewire/Users/Index.php
+++ app/Http/Livewire/Users/Index.php
@@ -147,19 +147,12 @@ class Index extends Component
     private function sendSuccessMail($token_code):void
     {
 
-        $body = "<p style='text-align:center; font-weight:bold'>Thank you,  {$this->fullname}</p>";
-        $body .= "<p style='text-align:center;'>You are all signed up for <b>The Zenith Tech Fair 2024.</b></p>";
-        $body .= "<p style='text-align:center; font-weight:bold'>Theme:<br> Embedded Financing, Cybersecurity & Growth Imperatives</p>";
-        $body .= "<p><b>Address: </b>The Civic Centre, Ozumba Mbadiwe, Victoria Island, Lagos.</p>";
-        $body .= "<p><b>Access Code: </b>{$token_code}.</p>";
-        $body .= "<p><b>Date: </b>Thursday, November 21, 2024.</p>";
-        $body .= "<p><b>Time: </b>8:00 am</p>";
-
         $payload = [
             'username' => $this->fullname,
+            'fullname' => $this->fullname,
             'email' => $this->email,
             'subject' => "{$this->fullname}, thank you for registering for the event",
-            'body' => $body,
+//            'body' => $body,
             'token_code'=>$token_code
         ];
 
--- storage/framework/views/e7bad40f4296b0cd6ef4e47c1d44a4c20e0d816e.php
+++ storage/framework/views/e7bad40f4296b0cd6ef4e47c1d44a4c20e0d816e.php
@@ -2,10 +2,14 @@
 <div style="text-align: center; margin-bottom: 5%">
     <img  src="https://zbtechfair.com/assets/images/logo_black.png"  alt="">
 </div>
-
-<?php echo $data->body; ?>
-
-
+<p style='text-align:center; font-weight:bold'>Thank you,  <?php echo e($data->fullname); ?></p>
+<p style='text-align:center;'>You are all signed up for <b>The Zenith Tech Fair 2024.</b></p>
+<p style='text-align:center; font-weight:bold'>Theme:<br> Embedded Financing, Cybersecurity & Growth Imperatives</p>
+<p><b>Address: </b>The Civic Centre, Ozumba Mbadiwe, Victoria Island, Lagos.</p>
+<p><b>Access Code: </b><?php echo e($data->token_code); ?>.</p>
+<p><b>Date: </b>Thursday, November 21, 2024.</p>
+<p><b>Time: </b>8:00 am</p>
+<div style='text-align:center'><img src='<?php echo e(url('qrcode/'.$data->token_code)); ?>' alt='<?php echo e($data->token_code); ?>.jpg' style='width:50%' /></div>
 <br>
 <p>You will need to come to the venue with the QR code for access.</p>
 <p>For any inquiries send us an email at <a href="mailto:zenithdirect@zenithbank.com">zenithdirect@zenithbank.com</a></p>
