775c9a391de88805c9680db85126a46b3b6bd3f1
ADMIN <olorundaolaoluwa@gmail.com>
2024-11-12 21:51:21 +0100

    The

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/verify_qr', function () {
+Route::any('/verify_qr', function () {
 
     $myArray = json_decode(request()->getContent(), TRUE);
     if ($myArray == '') {
