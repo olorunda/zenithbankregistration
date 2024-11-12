3f5252f978866805ee44182af8257e5ddd2e2b74
ADMIN <olorundaolaoluwa@gmail.com>
2024-11-12 21:12:56 +0100

    remove no

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Attendance;
+use App\Models\QrCode;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/verify_qr', function () {
+
+    $code=request()->code;
+    $this->details = QrCode::with(['registration', 'attendance'])->where('token', cleaner($code))->first();
+    if (request()->token != '56TYbbbyuebujefn9902b') {
+        return response()->json(['status' => 'error', 'message' => 'Invalid Authn Code , No entry'], 404);
+    }
+
+    if (is_null($this->details)) {
+        return response()->json(['status' => 'error', 'message' => 'Invalid QR Code , No entry'], 404);
+    }
+
+    Attendance::updateOrCreate(['registration_id' => $this->details->registration->id], [
+        'registration_id' => $this->details->registration->id,
+        'date_admitted' => now()
+    ]);
+
+    return response()->json(['status' => 'success', 'message' => 'Permission Granted']);
+
+
 });
