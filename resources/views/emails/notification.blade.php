5c7eca1af4b30e2857568e9f50375c4b4945ecc9
ADMIN <olorundaolaoluwa@gmail.com>
2024-11-13 20:48:38 +0100

    THE

--- resources/views/emails/notification.blade.php
+++ resources/views/emails/notification.blade.php
@@ -8,5 +8,5 @@
 <br>
 <p>This QR Code is required for access into the venue.</p>
 <p>For further enquiries, please contact us at <a href="mailto:zenithdirect@zenithbank.com">zenithdirect@zenithbank.com</a></p>
-<p>If you would like to open an account with us, click the link below <br><a href="https://onlineac.zenithbank.com/account-opening" target="_blank">https://acctgw.zenithbank.com/OnlineAccountOpening</a></p>
+<p>If you would like to open an account with us, click the link below <br><a href="https://onlineac.zenithbank.com/account-opening" target="_blank">https://onlineac.zenithbank.com/account-opening</a></p>
 @endcomponent
