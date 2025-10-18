1f13d91c0a3af6303a57dd4b19e3a42b4a08465c
olaoluwa <olorunda@markify.com>
2025-10-18 06:01:08 +0100

    balosh access control compatible

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+
 Route::any('/verify_qr', function () {
 
     if(\request()->method()=='POST'){
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Models\Attendance;
 use App\Models\QrCode;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,6 +24,35 @@ Route::get('/', function () {
 Route::get('/qr_image/{code}', function ($code) {
     return response()->file(storage_path("app/public/qrcode/$code"));
 
+});
+
+//balosh compatibility
+Route::post('/verify-access',function (){
+    $body = json_decode(request()->getContent(),true);
+    $response=Http::get('https://zbtechfair.com/api/verify_qr?token=56TYbbbyuebujefn9902b&code='.$body['accessCode'])->json()->toArray();
+
+    if($response['status']=='success') {
+        return response()->json([
+                                "accessGranted"=> true,
+                                "message"=> "Access authorized for entry"
+                                ]);
+    } else{
+        return response()->json([
+            "accessGranted"=> false,
+            "message"=> "Invalid or expired code"
+        ]);
+    }
+});
+
+
+
+Route::post('/access-notify',function (){
+    $body = request()->getContent();
+    Log::info(json_encode($body));
+    return response()->json([
+                        "received"=> true,
+                        "message"=> "Access event logged successfully"
+                        ]);
 });
 
 
